d99479d8f6083c5e908e90c190af861811cfafcc
Aaradhya06 <60058609+Aaradhya06@users.noreply.github.com>
2021-01-22 18:10:35 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,502 @@
+const dictionary = {
+    "door" : {
+    "word" : "door",
+    "inSanskrit": "dura",
+    "SanskritScript":"दुर्",
+    "definition" : "gate, door, entrance"
+  },
+  "beauty" : {
+    "word" : "beauty",
+    "inSanskrit": "saundarya",
+    "SanskritScript":"सौन्दर्य",
+    "definition" : "the quality which gives pleasure to the senses; the state of being beautiful"
+  },
+  "book" : {
+    "word" : "book",
+    "inSanskrit": "pusta",
+    "SanskritScript":"पुस्त",
+    "definition" : "a written work that is published as printed pages fastened together inside a cover, or in electronic form"
+  },
+  "home" : {
+    "word" : "home",
+    "inSanskrit": "	gRha",
+    "SanskritScript":"गृह",
+    "definition" : "the place where one lives permanently, especially as a member of a family or household."
+  },
+  "bird" : {
+    "word" : "bird",
+    "inSanskrit": "pakSi ",
+    "SanskritScript":"पक्षि",
+    "definition" : "a warm-blooded egg-laying vertebrate distinguished by the possession of feathers, wings, and a beak and (typically) by being able to fly."
+  },
+  "food" : {
+    "word" : "food",
+    "inSanskrit": "khAdana",
+    "SanskritScript":"खादन",
+    "definition" : "any nutritious substance that people or animals eat or drink or that plants absorb in order to maintain life and growth."
+  },
+  "water" : {
+    "word" : "water",
+    "inSanskrit": "jala",
+    "SanskritScript":"जल",
+    "definition" : "a colorless, transparent, odorless liquid that forms the seas, lakes, rivers, and rain and is the basis of the fluids of living organisms."
+  },
+  "human" : {
+    "word" : "human",
+    "inSanskrit": "mAnuSyaka",
+    "SanskritScript":"मानुष्यक",
+    "definition" : "relating to or characteristic of people or human beings."
+  },
+  "chair" : {
+    "word" : "chair",
+    "inSanskrit": "	Asanda",
+    "SanskritScript":"आसन्द",
+    "definition" : "a separate seat for one person, typically with a back and four legs."
+  },
+  "car" : {
+    "word" : "car",
+    "inSanskrit": "svayampreritavAhana",
+    "SanskritScript":"स्वयम्प्रेरितवाहन",
+    "definition" : "a four-wheeled road vehicle that is powered by an engine and is able to carry a small number of people."
+  },
+  "slow" : {
+    "word" : "slow",
+    "inSanskrit": "मन्द",
+    "SanskritScript":"मन्द",
+    "definition" : "moving or operating, or designed to do so, only at a low speed; not quick or fast."
+  },
+  "fast" : {
+    "word" : "fast",
+    "inSanskrit": "zIghra",
+    "SanskritScript":"शीघ्र",
+    "definition" : "moving or capable of moving at high speed."
+  },
+  "fool" : {
+    "word" : "fool",
+    "inSanskrit": "mUrkha",
+    "SanskritScript":"मूर्ख",
+    "definition" : "a person who acts unwisely or imprudently; a silly person"
+  },
+  "girl" : {
+    "word" : "girl",
+    "inSanskrit": "	kanyA",
+    "SanskritScript":"कन्या",
+    "definition" : "a female child."
+  },
+  "boy" : {
+    "word" : "boy",
+    "inSanskrit": "	bAlaka",
+    "SanskritScript":"बालक",
+    "definition" : "a male child or young man."
+  },
+  "air" : {
+    "word" : "air",
+    "inSanskrit": "	vAyu",
+    "SanskritScript":"वायु",
+    "definition" : "the invisible gaseous substance surrounding the earth, a mixture mainly of oxygen and nitrogen."
+  },
+  "man" : {
+    "word" : "man",
+    "inSanskrit": "पुरुष",
+    "SanskritScript":"पुरुष",
+    "definition" : "an adult male human being."
+  },
+  "woman" : {
+    "word" : "woman",
+    "inSanskrit": "strI",
+    "SanskritScript":"स्त्री",
+    "definition" : "an adult female human being."
+  },
+  "milk" : {
+    "word" : "milk",
+    "inSanskrit": "	kSIra",
+    "SanskritScript":"क्षीर",
+    "definition" : "an opaque white fluid rich in fat and protein, secreted by female mammals for the nourishment of their young."
+  },
+  "bed" : {
+    "word" : "bed",
+    "inSanskrit": "zayana",
+    "SanskritScript":"शयन",
+    "definition" : "a piece of furniture for sleep or rest, typically a framework with a mattress and coverings."
+  },
+  "education" : {
+    "word" : "education",
+    "inSanskrit": "zikSaNa",
+    "SanskritScript":"शिक्षण",
+    "definition" : "information about or training in a particular field or subject."
+  },
+  "near" : {
+    "word" : "near",
+    "inSanskrit": "samIpa",
+    "SanskritScript":"समीप",
+    "definition" : "at or to a short distance away; nearby."
+  },
+  "far" : {
+    "word" : "far",
+    "inSanskrit": "dUra",
+    "SanskritScript":"दूर",
+    "definition" : "at, to, or by a great distance"
+  },
+  "tasty" : {
+    "word" : "tasty",
+    "inSanskrit": "svAdanIya",
+    "SanskritScript":"स्वादनीय",
+    "definition" : " having a pleasant, distinct flavor."
+  },
+  "one" : {
+    "word" : "one",
+    "inSanskrit": "ekam",
+    "SanskritScript":"एकम्",
+    "definition" : "the lowest cardinal number; half of two; 1."
+  },
+  "two" : {
+    "word" : "two",
+    "inSanskrit": "dvau",
+    "SanskritScript":"द्वौ",
+    "definition" : "equivalent to the sum of one and one; one less than three; 2."
+  },
+  "three" : {
+    "word" : "three",
+    "inSanskrit": "trINi",
+    "SanskritScript":"त्रीणि",
+    "definition" : "equivalent to the sum of one and two; one more than two; 3."
+  },
+  "ten" : {
+    "word" : "ten",
+    "inSanskrit": "daśa",
+    "SanskritScript":"दश",
+    "definition" : "equivalent to the product of five and two; one more than nine; 10."
+  },
+  "hundred" : {
+    "word" : "hundred",
+    "inSanskrit": "	zata",
+    "SanskritScript":"शत",
+    "definition" : "the number equivalent to the product of ten and ten; ten more than ninety; 100."
+  },
+  "thousand" : {
+    "word" : "thousand",
+    "inSanskrit": "sahasra",
+    "SanskritScript":"सहस्र",
+    "definition" : "the number equivalent to the product of a hundred and ten; 1,000."
+  },
+  "animal" : {
+    "word" : "animal",
+    "inSanskrit": "	pazu",
+    "SanskritScript":"पशु",
+    "definition" : "a living organism that feeds on organic matter, typically having specialized sense organs and nervous system and able to respond rapidly to stimuli."
+  },
+  "plant" : {
+    "word" : "plant",
+    "inSanskrit": "vRkSaka",
+    "SanskritScript":"वृक्षक",
+    "definition" : "a living organism of the kind exemplified by trees, shrubs, herbs, grasses, ferns, and mosses, typically growing in a permanent site, absorbing water and inorganic substances through its roots, and synthesizing nutrients in its leaves by photosynthesis using the green pigment chlorophyll."
+  },
+  "tree" : {
+    "word" : "tree",
+    "inSanskrit": "	vRkSa",
+    "SanskritScript":"वृक्ष",
+    "definition" : "a woody perennial plant, typically having a single stem or trunk growing to a considerable height and bearing lateral branches at some distance from the ground."
+  },
+  "train" : {
+    "word" : "train",
+    "inSanskrit": "relayAna",
+    "SanskritScript":"रेलयान",
+    "definition" : "a series of railroad cars moved as a unit by a locomotive or by integral motors."
+  },
+  "fire" : {
+    "word" : "fire",
+    "inSanskrit": "agni",
+    "SanskritScript":"अग्नि",
+    "definition" : "combustion or burning, in which substances combine chemically with oxygen from the air and typically give out bright light, heat, and smoke."
+  },
+  "hot" : {
+    "word" : "hot",
+    "inSanskrit": "ushNa",
+    "SanskritScript":"उष्ण",
+    "definition" : "having a high degree of heat or a high temperature."
+  },
+  "cold" : {
+    "word" : "cold",
+    "inSanskrit": "zaitya",
+    "SanskritScript":"शैत्य",
+    "definition" : "of or at a low or relatively low temperature"
+  },
+  "tool" : {
+    "word" : "tool",
+    "inSanskrit": "upkaraNa",
+    "SanskritScript":"उपकरण",
+    "definition" : "a device or implement, especially one held in the hand, used to carry out a particular function"
+  },
+  "word" : {
+    "word" : "word",
+    "inSanskrit": "shabda",
+    "SanskritScript":"शब्द",
+    "definition" : "a single distinct meaningful element of speech or writing"
+  },
+  "window" : {
+    "word" : "window",
+    "inSanskrit": "	vAtAyana",
+    "SanskritScript":"वातायन",
+    "definition" : "an opening in the wall or roof of a building or vehicle that is fitted with glass or other transparent material in a frame to admit light or air and allow people to see out."
+  },
+  "light" : {
+    "word" : "light",
+    "inSanskrit": "prakasha",
+    "SanskritScript":"प्रकाश",
+    "definition" : "the natural agent that stimulates sight and makes things visible."
+  },
+  "dark" : {
+    "word" : "dark",
+    "inSanskrit": "	timira",
+    "SanskritScript":"तिमिर",
+    "definition" : "with little or no light."
+  },
+  "intelligent" : {
+    "word" : "intelligent",
+    "inSanskrit": "	buddhimAn",
+    "SanskritScript":"बुद्धिमान्",
+    "definition" : "having or showing intelligence, especially of a high level."
+  },
+  "power" : {
+    "word" : "power",
+    "inSanskrit": "	shakti",
+    "SanskritScript":"स्हक्ति",
+    "definition" : "the ability to do something or act in a particular way, especially as a faculty or quality."
+  },
+  "god" : {
+    "word" : "god",
+    "inSanskrit": "paramezvara",
+    "SanskritScript":"परमेश्वर",
+    "definition" : "the creator and ruler of the universe and source of all moral authority; the supreme being."
+  },
+  "teacher" : {
+    "word" : "teacher",
+    "inSanskrit": "adhyApaka",
+    "SanskritScript":"अध्यापक",
+    "definition" : "a person who teaches, especially in a school."
+  },
+  "knowledge" : {
+    "word" : "knowledge",
+    "inSanskrit": "	vidyA",
+    "SanskritScript":"विद्या",
+    "definition" : "facts, information, and skills acquired by a person through experience or education; the theoretical or practical understanding of a subject."
+  },
+  "music" : {
+    "word" : "music",
+    "inSanskrit": "	saGgIta",
+    "SanskritScript":"सङ्गीत",
+    "definition" : "vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."
+  },
+  "clever" : {
+    "word" : "clever",
+    "inSanskrit": "	catura",
+    "SanskritScript":"चतुर",
+    "definition" : "quick to understand, learn, and devise or apply ideas; intelligent."
+  },
+  "stop" : {
+    "word" : "stop",
+    "inSanskrit": "	zamana",
+    "SanskritScript":"शमन",
+    "definition" : " come to an end; cease to happen."
+  },
+  "cost" : {
+    "word" : "cost",
+    "inSanskrit": "	mUlya",
+    "SanskritScript":"मूल्य",
+    "definition" : "a specified sum of money"
+  },
+  "price" : {
+    "word" : "price",
+    "inSanskrit": "	mUlya",
+    "SanskritScript":"मूल्य",
+    "definition" : "a specified sum of money"
+  },
+  "brain" : {
+    "word" : "brain",
+    "inSanskrit": "	mastiSka",
+    "SanskritScript":"मस्तिष्क",
+    "definition" : "an organ of soft nervous tissue contained in the skull of vertebrates, functioning as the coordinating center of sensation and intellectual and nervous activity."
+  },
+  "important" : {
+    "word" : "important",
+    "inSanskrit": "mahatvapUrNa",
+    "SanskritScript":"महत्वपूर्ण",
+    "definition" : "of great significance or value; likely to have a profound effect on success, survival, or well-being."
+  },
+  "ball" : {
+    "word" : "ball",
+    "inSanskrit": "gola",
+    "SanskritScript":"गोल",
+    "definition" : "a solid or hollow spherical or egg-shaped object that is kicked, thrown, or hit in a game."
+  },
+  "earth" : {
+    "word" : "earth",
+    "inSanskrit": "	pRthvI",
+    "SanskritScript":"पृथ्वी",
+    "definition" : "the planet on which we live; the world"
+  },
+  "land" : {
+    "word" : "land",
+    "inSanskrit": "	bhUmi",
+    "SanskritScript":"भूमि",
+    "definition" : "the part of the earth's surface that is not covered by water, as opposed to the sea or the air."
+  },
+  "gold" : {
+    "word" : "gold",
+    "inSanskrit": "suvarNa",
+    "SanskritScript":"सुवर्ण",
+    "definition" : "a yellow precious metal, the chemical element of atomic number 79, used especially in jewelry and decoration and to guarantee the value of currencies."
+  },
+  "silver" : {
+    "word" : "silver",
+    "inSanskrit": "rajata",
+    "SanskritScript":"रजत",
+    "definition" : "a precious shiny grayish-white metal, the chemical element of atomic number 47."
+  },
+  "game" : {
+    "word" : "game",
+    "inSanskrit": "	khelA",
+    "SanskritScript":"क्रीडा",
+    "definition" : "a form of play or sport, especially a competitive one played according to rules and decided by skill, strength, or luck."
+  },
+  "colour" : {
+    "word" : "colour",
+    "inSanskrit": "	raGga",
+    "SanskritScript":"रङ्ग",
+    "definition" : "the property possessed by an object of producing different sensations on the eye as a result of the way the object reflects or emits light."
+  },
+  "stone" : {
+    "word" : "stone",
+    "inSanskrit": "	grAvan",
+    "SanskritScript":"ग्रावन्",
+    "definition" : "hard solid nonmetallic mineral matter of which rock is made, especially as a building material."
+  },
+  "rock" : {
+    "word" : "rock",
+    "inSanskrit": "shilA",
+    "SanskritScript":"शिला",
+    "definition" : "hard solid nonmetallic mineral matter of which rock is made, especially as a building material."
+  },
+  "sun" : {
+    "word" : "sun",
+    "inSanskrit": "sUrya",
+    "SanskritScript":"सूर्य",
+    "definition" : "the star around which the earth orbits."
+  },
+  "moon" : {
+    "word" : "moon",
+    "inSanskrit": "chandra",
+    "SanskritScript":"चन्द्र",
+    "definition" : "the natural satellite of the earth, visible (chiefly at night) by reflected light from the sun."
+  },
+  "sorry" : {
+    "word" : "sorry",
+    "inSanskrit": "kSamyatAm",
+    "SanskritScript":"क्षम्यताम्",
+    "definition" : "feeling distress, especially through sympathy with someone else's misfortune"
+  },
+  "come" : {
+    "word" : "come",
+    "inSanskrit": "	Agacchati",
+    "SanskritScript":"आगच्छति",
+    "definition" : "move or travel toward or into a place thought of as near or familiar to the speaker."
+  },
+  "go" : {
+    "word" : "go",
+    "inSanskrit": "vicchati ",
+    "SanskritScript":"विच्छति ",
+    "definition" : "move from one place to another; travel."
+  },
+  "no" : {
+    "word" : "no",
+    "inSanskrit": "na",
+    "SanskritScript":"न",
+    "definition" : "not any."
+  },
+  "yes" : {
+    "word" : "yes",
+    "inSanskrit": "	bhavatu!",
+    "SanskritScript":"भवतु!",
+    "definition" : "used to give an affirmative response."
+  },
+  "voice" : {
+    "word" : "voice",
+    "inSanskrit": "	dhvani",
+    "SanskritScript":"ध्वनि",
+    "definition" : "the sound produced in a person's larynx and uttered through the mouth, as speech or song."
+  },
+  "noise" : {
+    "word" : "noise",
+    "inSanskrit": "kolAhala",
+    "SanskritScript":"कोलाहल",
+    "definition" : "a sound, especially one that is loud or unpleasant or that causes disturbance."
+  },
+  "life" : {
+    "word" : "life",
+    "inSanskrit": "	jIvana",
+    "SanskritScript":"जीवन",
+    "definition" : "the condition that distinguishes animals and plants from inorganic matter, including the capacity for growth, reproduction, functional activity, and continual change preceding death."
+  },
+  "death" : {
+    "word" : "death",
+    "inSanskrit": "	vyApad",
+    "SanskritScript":"मरण",
+    "definition" : "the action or fact of dying or being killed; the end of the life of a person or organism."
+  },
+  "name" : {
+    "word" : "name",
+    "inSanskrit": "nAma",
+    "SanskritScript":"नाम",
+    "definition" : "a word or set of words by which a person, animal, place, or thing is known, addressed, or referred to"
+  },
+  "picture" : {
+    "word" : "picture",
+    "inSanskrit": "	citra",
+    "SanskritScript":"चित्र",
+    "definition" : "a representation of the external form of a person or thing in art."
+  },
+  "image" : {
+    "word" : "image",
+    "inSanskrit": "	citra",
+    "SanskritScript":"चित्र",
+    "definition" : "a representation of the external form of a person or thing in art."
+  },
+  "photo" : {
+    "word" : "photo",
+    "inSanskrit": "	citra",
+    "SanskritScript":"चित्र",
+    "definition" : "a representation of the external form of a person or thing in art."
+  },
+  "history" : {
+    "word" : "history",
+    "inSanskrit": "	itihAsa",
+    "SanskritScript":"इतिहास",
+    "definition" : "the study of past events, particularly in human affairs."
+  },
+  "break" : {
+    "word" : "break",
+    "inSanskrit": "nigrAha",
+    "SanskritScript":"निग्राह",
+    "definition" : "separate or cause to separate into pieces as a result of a blow, shock, or strain"
+  },
+  "please" : {
+    "word" : "please",
+    "inSanskrit": "prasidayati",
+    "SanskritScript":"प्रसिदयति",
+    "definition" : "cause to feel happy and satisfied."
+  },
+  "thanks" : {
+    "word" : "thanks",
+    "inSanskrit": " dhanyavAdAH",
+    "SanskritScript":"धन्यवादाः",
+    "definition" : "an expression of gratitude."
+  },
+  "thank you" : {
+    "word" : "thank you",
+    "inSanskrit": " dhanyavAdAH",
+    "SanskritScript":"धन्यवादाः",
+    "definition" : "an expression of gratitude."
+  },
+
+}
+export default dictionary
